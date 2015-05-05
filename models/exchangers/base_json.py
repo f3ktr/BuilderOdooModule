bf9b564d646217b5bfe47f9bc99598d054dad6ec
Yunier Rojas García <yunier.rojas@gmail.com>
2015-05-05 17:34:28 -0400

    minor changes

--- models/exchangers/base_json.py
+++ models/exchangers/base_json.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 import json
-
+from psycopg2._psycopg import IntegrityError
+from openerp.exceptions import except_orm
 from openerp import models, api
 
 
@@ -16,9 +18,9 @@ class OdooBuilderTranslator(object):
                 for name, column in obj._model._columns.items():
                     if name in ['id', 'write_uid', 'write_date', 'create_date', 'create_uid']:
                         continue
-                    if getattr(column, '_fnct', False) or not getattr(column, 'store', True):
+                    if column._type in ['function'] or getattr(column, '_fnct', False) or not getattr(column, 'store', True):
                         continue
-                    if column._type in ['char', 'boolean', 'integer', 'reference', 'text', 'html', 'float', 'date', 'datetime', 'selection', 'binary']:
+                    if column._type in ['char', 'boolean', 'integer', 'text', 'html', 'float', 'date', 'datetime', 'selection', 'binary']:
                         instance[name] = getattr(obj, name)
                     else:
                         instance[name] = getattr(self, 'handle_model_{type}'.format(type=column._type))(obj, name)
@@ -40,6 +42,98 @@ class OdooBuilderTranslator(object):
     def handle_model_many2one(self, obj, name):
         return self.translate(getattr(obj, name))
 
+    def handle_model_reference(self, obj, name):
+        return self.translate(getattr(obj, name))
+
+
+def model_required_attributes(model):
+    columns = model._columns
+    return {
+        name
+        for name, column in columns.items()
+        if getattr(column, 'required', False) and getattr(column, 'store', True) and not getattr(column, '_fnct', False)
+    }
+
+
+class OdooBuilderLoader(object):
+    def __init__(self, env=None):
+        self.seen_models = {}
+        self.env = env
+
+    def get_objects(self, data, pack=None):
+        pack = pack or defaultdict(dict)
+        if isinstance(data, dict):
+            if '@model' in data and '@id' in data:
+                model_str = data['@model']
+                id_str = data['@id']
+                pack[model_str, id_str].update({
+                    key: value if not isinstance(value, dict) else {
+                        '@model': value.get('@model'),
+                        '@id': value.get('@id'),
+                    }
+                    for key, value in data.items()
+                    if key not in ['@model', '@id']
+                })
+            [
+                self.get_objects(data[attr], pack)
+                for attr in data
+            ]
+        elif isinstance(data, list):
+            [
+                self.get_objects(item, pack)
+                for item in data
+            ]
+        return pack
+
+    def create_objects(self, objects):
+        changes = True
+
+        while objects and changes:
+            changes = False
+            for obj_key in objects.keys():
+                model_str, id_str = obj_key
+                model = self.env[model_str]
+                data = objects[obj_key]
+                missing = [
+                    key for key, value in data.items() if isinstance(value, dict) and value['@model'].startswith('builder.') and not self.seen_models.get((value['@model'], value['@id']))
+                ]
+
+                required_attributes = model_required_attributes(model)
+
+                if required_attributes.issubset(set(data.keys())) and not missing:
+                    obj = model.create({
+                        key: value if not isinstance(value, dict) else '{model},{id}'.format(model=value['@model'], id=self.seen_models.get((value['@model'], value['@id']))) if model._columns[key]._type == 'reference' else self.seen_models.get((value['@model'], value['@id']))
+                        for key, value in data.items()
+                        if not isinstance(value, list)
+                    })
+                    if obj:
+                        self.seen_models[obj_key] = obj.id
+                        changes = True
+                        objects.pop(obj_key)
+
+    def get_object(self, data):
+        return self.seen_models[data['@model'], data['@id']]
+
+    def build_relations(self, data):
+        if data and isinstance(data, dict):
+            if '@model' in data and '@id' in data:
+                model_str = data['@model']
+                id_str = data['@id']
+                model = self.env[model_str]
+                obj = self.seen_models[model_str, id_str]
+                columns = model._columns
+                for attr in data.keys():
+                    if attr in columns:
+                        if columns[attr]._type == 'many2many':
+                            obj.write({
+                                attr: [[6, False, [self.get_object(item).id for item in data[attr]]]]
+                            })
+
+    def load(self, data):
+        objects = self.get_objects(data)
+        self.create_objects(objects)
+        self.build_relations(data)
+
 
 class JSONExchanger(models.Model):
     _name = 'builder.exchanger.json'
@@ -58,3 +152,10 @@ class JSONExchanger(models.Model):
     def export_module(self, module):
         translator = OdooBuilderTranslator()
         return json.dumps(translator.translate(module), sort_keys=True, indent=4)
+
+    @api.model
+    def load_module(self, module):
+        data = json.loads(module)
+        translator = OdooBuilderLoader(self.env)
+        translator.load(data)
+
